7163387b2008b149449995b6ccb6eaa6ffb98914
Łukasz Podlipski <lukaszpodlipskikontakt@gmail.com>
2022-09-04 19:48:29 +0200

    Add fade in animation to title

--- src/components/Navigation/NavigationHeader.vue
+++ src/components/Navigation/NavigationHeader.vue
@@ -40,6 +40,16 @@ export default {
   }
   .page-title {
     color: var(--v-primary-lighten1);
+    animation: fadeIn 0.3s ease-in-out;
+  }
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
   }
 }
 .v-icon::after {
